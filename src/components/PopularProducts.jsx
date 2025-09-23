090c079231f38c55f9b5e72de79e00efaeef44ec
pranaygoud01 <pranaygoudpalle123@gmail.com>
2025-09-23 14:06:32 +0530

    Added

--- src/components/PopularProducts.jsx
+++ src/components/PopularProducts.jsx
@@ -1,7 +1,7 @@
 import { Link } from "@tanstack/react-router";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-
+import { FaArrowRight } from "react-icons/fa6";
 const PopularProducts = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -37,48 +37,49 @@ const PopularProducts = () => {
   return (
     <section className="w-full px-6 md:px-20 py-12 max-lg:py-8">
       {/* Heading */}
-      <h2 className="mb-6 text-2xl font-bold text-gray-900 dark:text-gray-100 text-center sm:text-left">
-        Popular Products
-      </h2>
+      <div className="flex justify-between mb-6 items-center">
+        <h2 className=" text-2xl font-bold text-gray-900 :text-gray-100 text-left  max-lg:text-xl sm:text-left">
+          Popular Products
+        </h2>
+        <a className="text-xs font-semibold bg-black text-white px-3 py-2 rounded-md cursor-pointer flex  gap-1 items-center" href="/browse">See More<span><FaArrowRight/></span></a>
+      </div>
 
       {/* Horizontal Scroll */}
       <div className="w-full overflow-x-auto scrollbar-hide">
         <div className="flex gap-6 min-w-max">
-          {loading
-            ? Array.from({ length: 6 }).map((_, i) => <SkeletonCard key={i} />)
-            : products.length > 0
-            ? products.map((product) => (
-                <Link
-                  to={`/browse/product/${product._id}`}
-                  key={product._id}
-                  className="flex flex-col relative  border border-neutral-300 w-[300px] overflow-hidden rounded-lg bg-white dark:bg-gray-800 shadow-sm hover:shadow-md transition-shadow duration-300 min-w-[200px]"
-                >
-                  <div
-                    className="h-[300px]  bg-cover bg-center transition-transform duration-300 hover:scale-105"
-                    style={{ backgroundImage: `url(${product.image})` }}
-                  >
-                    
-                  </div>
-                  <p className="absolute top-3 font-semibold text-xs bg-red-800 rounded-md text-white px-2 py-1 left-3">{product?.campus?.name}</p>
+          {loading ? (
+            Array.from({ length: 6 }).map((_, i) => <SkeletonCard key={i} />)
+          ) : products.length > 0 ? (
+            products.map((product) => (
+              <Link
+                to={`/browse/product/${product._id}`}
+                key={product._id}
+                className="flex flex-col relative max-lg:w-[200px]  border border-neutral-300 w-[300px] overflow-hidden rounded-lg bg-white dark:bg-gray-800 shadow-sm hover:shadow-md transition-shadow duration-300 min-w-[200px]"
+              >
+                <div
+                  className="h-[300px] max-lg:h-[200px]  bg-cover bg-center transition-transform duration-300 hover:scale-105"
+                  style={{ backgroundImage: `url(${product.image})` }}
+                ></div>
+                <p className="absolute top-3 font-semibold text-xs max-lg:text-[9px] bg-red-900 rounded-md text-white px-2 py-1 left-3">
+                  {product?.campus?.name}
+                </p>
 
-                  <div className="flex flex-col p-3">
-                    <h3 className="font-medium text-gray-900 dark:text-gray-100 truncate">
-                      {product.name}
-                    </h3>
-                    <p className="text-sm text-gray-500 dark:text-gray-400 line-clamp-2">
-                      {product.description}
-                    </p>
-                    <p className="mt-2 text-lg font-semibold text-black">
-                      ₹{product.price}
-                    </p>
-                  </div>
-                </Link>
-              ))
-            : (
-              <div className="text-center mt-10 w-full">
-                No products found
-              </div>
-            )}
+                <div className="flex flex-col p-3">
+                  <h3 className="font-medium max-lg:text-sm text-gray-900 dark:text-gray-100 truncate">
+                    {product.name}
+                  </h3>
+                  <p className="text-sm max-lg:text-xs text-gray-500 dark:text-gray-400 line-clamp-2">
+                    {product.description}
+                  </p>
+                  <p className="mt-2 text-lg max-lg:text-sm font-semibold text-black">
+                    ₹{product.price}
+                  </p>
+                </div>
+              </Link>
+            ))
+          ) : (
+            <div className="text-center mt-10 w-full">No products found</div>
+          )}
         </div>
       </div>
     </section>
